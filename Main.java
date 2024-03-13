8606a61067581f407773e777278ead8a47d449f6
Harsh Ranjan <96332194+harsranjan@users.noreply.github.com>
2024-03-13 13:38:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,25 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("This is DevOps lab experiment Github part-2.");
+		//below code is done by harsh ranjan
+		static void sortarraybubblebyharsh(int[] arr)
+	{
+	    int length = arr.length;
+	    
+	    for(int i=0;i<length;i++)
+	    {
+	        for(int j =i+1;j<length;j++)
+	        {
+	            if(arr[i]>arr[j])
+	            {
+	                int temp;
+	                temp= arr[i];
+	                arr[i]=arr[j];
+	                arr[j]= temp;
+	            }
+	        }
+	    }
+	}
     }
 }
 
