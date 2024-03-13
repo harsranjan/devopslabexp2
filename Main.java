4a0582ec2a46e5a6ee312ae49f112916e88ed037
Girish-kumar23 <144627491+Girish-kumar23@users.noreply.github.com>
2024-03-13 13:47:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -41,6 +41,58 @@ public class Main
 
 }
 
+// Contributed by Girish Kumar
+
+public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6, 7};
+        System.out.println("Original array:");
+        printArray(array);
+
+        // Call the quickSort function
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Quick Sort Function
+    
+    static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
+
 		
 	
 
