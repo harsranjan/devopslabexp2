c91333f4f264b212a0c623c402775cafe323d424
Girish-kumar23 <144627491+Girish-kumar23@users.noreply.github.com>
2024-03-13 13:32:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,54 @@ public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("This is DevOps lab experiment Github part-2.");
+int[] array = {12, 11, 13, 5, 6, 7};
+        System.out.println("Original array:");
+        printArray(array);
 
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Quick Sort Function
+    // Contributed by Girish Kumar
+    static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
 
 		
-	}
-}
+	
 
