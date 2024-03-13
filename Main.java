a6263e83fcae8d616a6abbf49471ce4104eb4d36
gitikathakur <95364818+gitikathakur@users.noreply.github.com>
2024-03-13 13:55:06 +0530

    Update Main.java
    Gitika

--- Main.java
+++ Main.java
@@ -116,7 +116,31 @@ public static void main(String[] args) {
 		//end of the function
 		
 }
+// Contributed by Gitika Thakur
+         public class PrimeNumbers {
+    public static void main(String[] args) {
+        int limit = 50; 
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int num = 2; num <= limit; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
 
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 }
 
 		
